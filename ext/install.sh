3e8854a1de4bc83748f8e558bd7d9636cc9a1a66
cch <srdgame@gmail.com>
2013-02-05 23:57:40 +0800

    install standard ext modules by script

---
+++ ext/install.sh
@@ -0,0 +1,7 @@
+
+DST_PATH=/usr/lib/luasp
+cp luacurl/luacurl.so $DST_PATH
+cp luajson/luajson.so $DST_PATH
+cp luamemcache/luamcache.so $DST_PATH
+cp luamysql/luamysql.so $DST_PATH
+cp luaredis/luaredis.so $DST_PATH
